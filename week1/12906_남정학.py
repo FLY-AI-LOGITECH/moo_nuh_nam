3947100892ffa21ec7f5def307a6531b3229c23c
moo_nuh_nam <njh20000@naver.com>
2025-01-16 16:37:20 +0900

    ewre

--- week1/12906_남정학.py
+++
@@ -1,8 +0,0 @@
-def solution(arr):
-    answer = []
-    leng = len(arr)
-    answer.append(arr[0])
-    for i in range(1,leng):
-        if arr[i]!=answer[-1]:
-            answer.append(arr[i])
-    return answer
