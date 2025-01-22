109a00a1b85669dce7ae366e197a65ba543f061a
moo_nuh_nam <110164568+moonuhnam@users.noreply.github.com>
2025-01-22 16:24:58 +0900

    [level 1] Title: 공원 산책, Time: 0.52 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -0,0 +1,30 @@
+def solution(park, routes):
+    answer = []
+    
+    for i in range(len(park)):
+        for j in range(len(park[0])):
+            if park[i][j] == 'S':
+                sc, sr = i, j  
+                break
+
+    for i in range(len(routes)):
+        count = 0  
+        direction = routes[i][0] 
+        count = int(routes[i][2])  
+        rc, rr = sc, sr  
+
+        for _ in range(count):
+            if direction == 'E': 
+                sr += 1
+            elif direction == 'W':  
+                sr -= 1
+            elif direction == 'S':  
+                sc += 1
+            elif direction == 'N':  
+                sc -= 1
+
+            if sc < 0 or sc >= len(park) or sr < 0 or sr >= len(park[0]) or park[sc][sr] == 'X':
+                sc, sr = rc, rr
+                break
+
+    return [sc, sr]
